6053924cce8046f8d9a5fcb2229816b2cb47677f
Maleksafi <44367526+Maleksafi@users.noreply.github.com>
2022-01-16 21:35:35 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hup-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t malek1234/testt:3.3.3 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push malek1234/testt:3.3.3'
